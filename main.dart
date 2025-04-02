77aa8f261a5ef9636252b6712d10fabacb06eb16
sigit1202 <163863271+sigit1202@users.noreply.github.com>
2025-04-02 18:32:58 +0700

    Create main.dart

---
+++ main.dart
@@ -0,0 +1,22 @@
+// File: lib/main.dart
+import 'package:flutter/material.dart';
+import 'pages/home_page.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Voice Search Flutter',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const HomePage(),
+    );
+  }
+}
